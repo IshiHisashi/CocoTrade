824e997ca4aefe9e57e51be3ed5a8b8b67e41624
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-06-05 04:14:34 -0700

    feat: TASK-88 - get the current market price, convert it, and post it to DB

---
+++ backend/scraper.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import cron from "node-cron";
+import dotenv from "dotenv";
+dotenv.config({ path: "../config.env" });
+
+const postData = async (priceUSD, pricePHP) => {
+  try {
+    const res = await axios.post("http://localhost:5555/marketprice", {
+      price_USD: priceUSD,
+      price_PHP: pricePHP,
+    });
+    console.log("data posted successfully", res.data);
+  } catch (error) {
+    console.log("Error posting data: ", error.message);
+  }
+};
+
+const convertCurrency = async (priceUSD) => {
+  try {
+    const res = await fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGEAPI}/latest/USD`
+    );
+    const data = await res.json();
+    const pricePHP = priceUSD * data.conversion_rates.PHP;
+    return pricePHP;
+  } catch (error) {
+    console.log("Error converting currency: ", error.message);
+  }
+};
+
+const scraper = async () => {
+  try {
+    const res = await axios.get(
+      "https://markets.businessinsider.com/commodities/palm-oil-price"
+    );
+
+    const $ = cheerio.load(res.data);
+
+    const price = $(".price-section__current-value").text();
+    const priceUSD = Number(price);
+    return priceUSD;
+  } catch (error) {
+    console.log("Error scraping data: ", error.message);
+  }
+};
+
+// schedule to execute everyday at 0 o'clock.
+cron.schedule("0 0 0 * * *", async () => {
+  const priceUSD = await scraper();
+  const pricePHP = await convertCurrency(priceUSD);
+  postData(priceUSD, pricePHP);
+});

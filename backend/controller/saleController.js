00d2cc5055512f32437c9946a6447c0aaca9880f
Shehani Wijetunga <77097408+shehani-wijetunga@users.noreply.github.com>
2024-06-06 22:22:20 -0700

    Sales log  API
    I have added sales log add, view, update and delete endpoints and all working in postman and updating in db too

--- backend/controller/saleController.js
+++ backend/controller/saleController.js
@@ -1,4 +1,6 @@
-import { Sale } from "../model/saleModel.js";
+import {
+    Sale
+} from "../model/saleModel.js";
 
 export const createSale = async (req, res) => {
     try {
@@ -6,9 +8,10 @@ export const createSale = async (req, res) => {
         const savedSales = await newSales.save();
         console.log("Sale Added!");
         res.status(201).json(savedSales);
-    }
-    catch(err) {
-        res.status(500).json({error: err.message});
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
     }
 };
 
@@ -17,47 +20,66 @@ export const getAllSales = async (req, res) => {
         const sales = await Sale.find();
         console.log("Sales retrieved");
         res.status(200).json(sales);
-    }
-    catch (err) {
-        res.status(500).json({error: err.message});
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
     }
 };
 
 export const getSaleById = async (req, res) => {
     try {
         const sales = await Sale.findById(req.params.id);
-        if(!sales) {
-            return res.status(404).json({ error: 'Sale not found' });
+        if (!sales) {
+            return res.status(404).json({
+                error: 'Sale not found'
+            });
         }
         res.status(200).json(sales);
-    }
-    catch (err) {
-        res.status(500).json({error: err.message});
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
     }
 };
 
 export const updateSale = async (req, res) => {
     try {
-        const updatedSales = Sale.findByIdAndUpdate(req.params.id);
+        const updatedSales = await Sale.findByIdAndUpdate(req.params.id, req.body);
         if (!updatedSales) {
-            return res.status(404).json({ error: "Sale not found" });
+            return res.status(404).json({
+                error: "Sale not found"
+            });
+        } else {
+            return res.status(201).json({
+                status: "success in updating",
+            });
         }
-        res.status(200).json(updatedSales);
-    }
-    catch (err) {
-        res.status(500).json({error: err.message});
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({
+            status: "fail",
+            message: err,
+        });
     }
 }
 
-export const deleteSale = async (res, req) => {
+export const deleteSale = async (req, res) => {
     try {
-        const deletedSales = await Sale.findByIdAndDelete(res.params.id);
+        const deletedSales = await Sale.findByIdAndDelete(req.params.id);
         if (!deletedSales) {
-            return res.status(404).json({ error: "Sale not found" });
+            return res.status(404).json({
+                error: "Sale not found"
+            });
         }
-        res.status(200).json(deletedSales);
-    }
-    catch (err) {
-        res.status(500).json({error: err.message});
+        return res.status(200).json({
+            status: "success",
+            message: "Sale successfully deleted"
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err.message
+        });
     }
 }

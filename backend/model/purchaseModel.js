2a0b5467b19daf756d961215d989ecdfe29b8195
Shehani Wijetunga <77097408+shehani-wijetunga@users.noreply.github.com>
2024-06-06 22:32:34 -0700

    Purchase logs API endpoints
    This commit is regarding adding endpoints for purchase logs: add, view, update and delete a purchase log. These all endpoints are tested in postman and checked in database.

--- backend/model/purchaseModel.js
+++ backend/model/purchaseModel.js
@@ -6,13 +6,11 @@ export {
 
 const Schema = mongoose.Schema;
 const purchaseSchema = new Schema({
-    purchase_log_id: String,
     user_id: Array,
     farmer_id: Array,
-    amount_of_copra_purchased: Number,
-    status: { type: String, enum: ['pending', 'ongoing', 'completed', 'cancelled'], default: 'pending' },
+    amount_of_copra_purchased: mongoose.Types.Decimal128,
     moisture_test_result: Boolean,
-    moisture_test_details: Number,
-    total_purchase_price: Decimal128
+    moisture_test_details: mongoose.Types.Decimal128,
+    total_purchase_price: mongoose.Types.Decimal128
 });
 const Purchase = mongoose.model('Purchase', purchaseSchema);

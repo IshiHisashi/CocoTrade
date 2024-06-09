81fbb7bddc8092eb36c55b29449725bcf7724ebb
IshiHisashi <ihsasihhi@gmail.com>
2024-06-08 22:46:05 -0700

    TASK-95:revise data type of user_id, farmer_id and manufacturer_id

--- backend/model/saleModel.js
+++ backend/model/saleModel.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-export {
-    Sale
-};
+export { Sale };
 
 const Schema = mongoose.Schema;
 const saleSchema = new Schema({
-    user_id: Array,
-    manufacturer_id: Array,
-    amount_of_copra_sold: mongoose.Types.Decimal128,
-    status: { type: String, enum: ['pending', 'ongoing', 'completed', 'cancelled'], default: 'pending' },
-    copra_ship_date: Date,
-    cheque_receive_date: Date,
-    total_sales_price: mongoose.Types.Decimal128
+  user_id: { type: Schema.Types.ObjectId, ref: "User" },
+  manufacturer_id: { type: Schema.Types.ObjectId, ref: "Manufacturer" },
+  amount_of_copra_sold: mongoose.Types.Decimal128,
+  status: {
+    type: String,
+    enum: ["pending", "ongoing", "completed", "cancelled"],
+    default: "pending",
+  },
+  copra_ship_date: Date,
+  cheque_receive_date: Date,
+  total_sales_price: mongoose.Types.Decimal128,
 });
-const Sale = mongoose.model('Sale', saleSchema);
+const Sale = mongoose.model("Sale", saleSchema);

fb7c6cae8c8913bd109242c27779ff2b0f843ea1
Shehani Wijetunga <77097408+shehani-wijetunga@users.noreply.github.com>
2024-06-08 18:13:43 -0700

    Task-96 -  commit changes from feedback
    delete purchase_lod_id : we don't need to have PK in the schema (id will automatically assign)
    change data type for amount_of_copra_purchased and moisture_test_details to decimal128
    delete status : it's not on out ERD for purchase log.
    The syntax must be "mongoose.Types.Decimal128" but not "Decimal128"

--- backend/model/saleModel.js
+++ backend/model/saleModel.js
@@ -6,13 +6,12 @@ export {
 
 const Schema = mongoose.Schema;
 const saleSchema = new Schema({
-    sale_log_id: String,
     user_id: Array,
     manufacturer_id: Array,
     amount_of_copra_sold: Number,
     status: { type: String, enum: ['pending', 'ongoing', 'completed', 'cancelled'], default: 'pending' },
     copra_ship_date: Date,
     cheque_receive_date: Date,
-    total_sales_price: Decimal128
+    total_sales_price: mongoose.Types.Decimal128
 });
 const Sale = mongoose.model('Sale', saleSchema);

5df861f8bcf77ca5b630ac54578125128ac9d5d3
IshiHisashi <ihsasihhi@gmail.com>
2024-06-09 00:22:32 -0700

    TASK-95:add purchase date

--- backend/model/purchaseModel.js
+++ backend/model/purchaseModel.js
@@ -6,6 +6,7 @@ const Schema = mongoose.Schema;
 const purchaseSchema = new Schema({
   user_id: { type: Schema.Types.ObjectId, ref: "User" },
   farmer_id: { type: Schema.Types.ObjectId, ref: "Farmer" },
+  purchase_date: Date,
   amount_of_copra_purchased: mongoose.Types.Decimal128,
   moisture_test_result: Boolean,
   moisture_test_details: mongoose.Types.Decimal128,

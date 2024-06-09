85f451fc4cb60fd81b1ad75147bd9bcae19fb8e7
Hisashi Ishihara <149908441+IshiHisashi@users.noreply.github.com>
2024-06-08 20:41:34 -0700

    Update saleModel.js

--- backend/model/saleModel.js
+++ backend/model/saleModel.js
@@ -8,7 +8,7 @@ const Schema = mongoose.Schema;
 const saleSchema = new Schema({
     user_id: Array,
     manufacturer_id: Array,
-    amount_of_copra_sold: Number,
+    amount_of_copra_sold: mongoose.Types.Decimal128,
     status: { type: String, enum: ['pending', 'ongoing', 'completed', 'cancelled'], default: 'pending' },
     copra_ship_date: Date,
     cheque_receive_date: Date,

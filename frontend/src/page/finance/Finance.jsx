30008f361a2be4daa769b1ba252fc38f79e85697
IshiHisashi <ihsasihhi@gmail.com>
2024-06-11 22:11:33 -0700

    TASK-122:build linechart for marketprice

--- frontend/src/page/finance/Finance.jsx
+++ frontend/src/page/finance/Finance.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import MonthlyActivity from "./MonthlyActivity.jsx";
+// import LineChart from "./LineChart.jsx";
 import LineChart from "./LineChart.jsx";
 
 const Finance = () => {
   const userId = "66654dc4c6e950671e988962";
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <h1>Finance</h1>
-      {/* <MonthlyActivity userId={userId} /> */}
+      <MonthlyActivity userId={userId} />
+      {/* <LineChart /> */}
       <LineChart />
     </div>
   );
--- frontend/src/page/finance/LineChart.jsx
+++ frontend/src/page/finance/LineChart.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import { Chart, registerables } from "chart.js";
-
-Chart.register(...registerables);
+import { Chart } from "chart.js";
+import "chartjs-adapter-moment";
 
 const LineChart = () => {
   const [marketPrice, setMarketPrice] = useState([]);
@@ -20,147 +19,156 @@ const LineChart = () => {
       .catch(console.log("waiting..."));
   }, []);
 
-  //   for line chart
   useEffect(() => {
     const ctxx = chartRef.current.getContext("2d");
-
-    if (chartInstanceRef.current) {
-      chartInstanceRef.current.destroy(); // Destroy existing chart instance
-    }
 
     const gradient = ctxx.createLinearGradient(0, 0, 0, 400);
     gradient.addColorStop(0, "rgba(75, 192, 192, 0.2)");
     gradient.addColorStop(1, "rgba(75, 192, 192, 0)");
 
-    const chartData = [
-      { date: "May 2023", price: 35.05 },
-      { date: "Jun 2023", price: 35.25 },
-      { date: "Jul 2023", price: 36.0 },
-      { date: "Aug 2023", price: 36.5 },
-      { date: "Sep 2023", price: 36.75 },
-      { date: "Oct 2023", price: 36.5 },
-      { date: "Nov 2023", price: 35.25 },
-      { date: "Dec 2023", price: 35.5 },
-      { date: "Jan 2024", price: 35.75 },
-      { date: "Feb 2024", price: 35.25 },
-      { date: "Mar 2024", price: 35.5 },
-      { date: "Apr 2024", price: 35.75 },
-      { date: "May 2024", price: 35.5 },
-    ];
+    const dailyData = marketPrice.map((price) => {
+      const priceObj = {
+        date: price.createdAt.slice(0, 10),
+        price: Number(price.price_PHP.$numberDecimal),
+      };
+      return priceObj;
+    });
 
     const data = {
-      labels: chartData.map((item) => item.date),
+      labels: [
+        "Jan 2024",
+        "Feb 2024",
+        "Mar 2024",
+        "Apr 2024",
+        "May 2024",
+        "Jun 2024",
+        "Jul 2024",
+        "Aug 2024",
+        "Sep 2024",
+        "Oct 2024",
+        "Nov 2024",
+        "Dec 2024",
+      ],
       datasets: [
         {
           label: "Market Price",
-          data: chartData.map((item) => item.price),
+          data: dailyData.map((point) => ({ x: point.date, y: point.price })),
           fill: true,
           backgroundColor: gradient,
           borderColor: "rgba(75, 192, 192, 1)",
           tension: 0.4,
-          pointRadius: 0, // Remove points
         },
       ],
+    };
+
+    const verticalLinePlugin = {
+      id: "verticalLinePlugin",
+      beforeDraw: (chart) => {
+        // eslint-disable-next-line no-underscore-dangle
+        if (chart.tooltip._active && chart.tooltip._active.length) {
+          const {
+            ctx,
+            scales: {
+              y: { top: topY, bottom: bottomY },
+            },
+            tooltip: {
+              _active: [
+                {
+                  element: { x },
+                },
+              ],
+            },
+          } = chart;
+
+          ctx.save();
+
+          ctx.beginPath();
+          ctx.moveTo(x, topY);
+          ctx.lineTo(x, bottomY);
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "#000000";
+          ctx.stroke();
+          ctx.restore();
+        }
+      },
     };
 
     const config = {
       type: "line",
       data,
       options: {
-        interaction: {
-          mode: "index", // Display tooltip for the nearest data point along the x-axis
-          intersect: false, // Tooltip appears even if not directly over a data point
-        },
+        responsive: true,
         plugins: {
+          legend: {
+            display: false,
+          },
           tooltip: {
+            mode: "index",
             intersect: false,
             callbacks: {
               label(context) {
-                const label = context.dataset.label || "";
-                const value = context.raw || "";
-                return `${label}: Php ${value}`;
-              },
-              title(context) {
-                return context[0].label;
+                const date = context.label;
+                const price = context.raw.y;
+                return `Php ${price.toFixed(2)}`;
               },
             },
           },
-          crosshairLine: {
-            lineColor: "rgba(0, 0, 0, 0.8)",
-            lineWidth: 1,
-          },
+        },
+        interaction: {
+          mode: "nearest",
+          axis: "x",
+          intersect: false,
         },
         scales: {
           x: {
+            type: "time",
+            time: {
+              unit: "month",
+              tooltipFormat: "yyyy-MM-dd",
+            },
             title: {
               display: true,
-              text: "Date",
+              text: "Month",
             },
             grid: {
-              display: false, // Remove grid lines
+              display: false,
             },
           },
           y: {
+            beginAtZero: false,
             title: {
               display: true,
-              text: "Price (Php)",
+              text: "Price",
             },
             grid: {
-              display: false, // Remove grid lines
+              display: false,
             },
-            beginAtZero: false,
           },
         },
         elements: {
-          point: {
-            radius: 0, // Hide points on the line
-          },
           line: {
-            borderWidth: 2, // Line thickness
+            tension: 0.4, // Smoothing effect
+          },
+          point: {
+            radius: 0, // Hide points
           },
         },
       },
-      plugins: [
-        {
-          id: "crosshairLine",
-          beforeDraw: (chart) => {
-            const tooltipItems = chart.tooltip?.getActiveElements();
-            if (tooltipItems?.length) {
-              const [
-                {
-                  element: { x },
-                },
-              ] = tooltipItems; // Destructure to avoid dangling underscore
-              const {
-                ctx,
-                scales: {
-                  y: { top: topY, bottom: bottomY },
-                },
-              } = chart; // Destructure ctx and scales
-
-              ctx.save();
-              ctx.beginPath();
-              ctx.moveTo(x, topY);
-              ctx.lineTo(x, bottomY);
-              ctx.lineWidth =
-                chart.config.options.plugins.crosshairLine.lineWidth;
-              ctx.strokeStyle =
-                chart.config.options.plugins.crosshairLine.lineColor;
-              ctx.stroke();
-              ctx.restore();
-            }
-          },
-        },
-      ],
+      plugins: [verticalLinePlugin],
     };
 
-    chartInstanceRef.current = new Chart(ctxx, config);
-  }, []);
+    const myChart = new Chart(ctxx, config);
+
+    // Cleanup on unmount
+    return () => {
+      myChart.destroy();
+    };
+  }, [marketPrice]);
 
   return (
-    <div>
-      LineChart
-      <canvas ref={chartRef} />
+    <div className="flex flex-col gap-8">
+      <p>LineChart </p>
+      <canvas ref={chartRef}> </canvas>
     </div>
   );
 };

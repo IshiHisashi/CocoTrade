6c24f97e7a88a2aa3541b420f9c716f0b62ae75d
Akiodesukedo <abtai0227aki@gmail.com>
2024-06-13 21:08:20 -0700

    build: barchart (bar missing lmao)

--- frontend/src/page/inventory/BarChart.jsx
+++ frontend/src/page/inventory/BarChart.jsx
@@ -1,48 +1,77 @@
 /* eslint-disable react/prop-types */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from "axios";
-import { Chart, BarController, BarElement, CategoryScale, LinearScale } from 'chart.js/auto';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 const BarChart = ({ userId }) => {
   const [inventory, setInventory] = useState([]);
   const [maximumInv, setMaximumInv] = useState(0);
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
 
   // Retreice info for bar chart
   useEffect(() => {
-    // Current inventory
+
+    // maximum capacity
     axios
-      .get(`http://localhost:5555/user/${userId}/inv`)
+      .get(`http://localhost:5555/user/${userId}/maxcap`)
       .then((res) => {
-          // setMaximumInv(res.data.data.max_amount);
-          setInventory(res.data.data[0].current_amount.$numberDecimal);
-          console.log(res.data.data[0].current_amount.$numberDecimal);
+          setMaximumInv(res.data.data);
+          console.log(res.data.data);
       })
       .catch((err) => {
           console.error(err);
-      })
-    // maximum capacity
+      });
+    // Current inventory
     axios
-    .get(`http://localhost:5555/user/${userId}/maxcap`)
+    .get(`http://localhost:5555/user/${userId}/inv`)
     .then((res) => {
-        setMaximumInv(res.data.data);
-        console.log(res.data.data);
+        // setMaximumInv(res.data.data.max_amount);
+        setInventory(res.data.data[0].current_amount.$numberDecimal);
+        console.log(res.data.data[0].current_amount.$numberDecimal);
     })
     .catch((err) => {
         console.error(err);
-    })    
+    });
   }, 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   [])
 
   useEffect(() => {
-    
-  }, [])
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
+
+    chartInstance.current = new Chart(chartRef.current, {
+      type: "bar",
+      data: {
+        label: ["aaa"],
+        datasets: [{
+          label: 'Inventory',
+          data: [inventory],
+          backgroundColor: "#0C7F8E",
+          barThickness: 30
+        }],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        },
+      },
+    });
+  },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [inventory])
 
   return (
     <div>
-      Barchart here
-      <h2>{ userId }</h2>
+      <h2>{ userId } inventory is like this now yay!</h2>
+      <canvas ref={chartRef}> </canvas>
     </div>
   )
 }

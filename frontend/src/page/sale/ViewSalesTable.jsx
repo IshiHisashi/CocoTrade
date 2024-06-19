b3f9e3ef2c409ee7857a433134d9a875578f83f2
Shehani Wijetunga <77097408+shehani-wijetunga@users.noreply.github.com>
2024-06-18 22:32:15 -0700

    Add edit sales log function

--- frontend/src/page/sale/ViewSalesTable.jsx
+++ frontend/src/page/sale/ViewSalesTable.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import EditSaleModal from './EditSaleModal';
 
-const ViewSalesTable = () => {
+const ViewSalesTable = ({ setShowAddForm, handleEdit })  => {
   const [sales, setSales] = useState([]);
   const [dropdownVisible, setDropdownVisible] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
@@ -21,18 +22,6 @@ const ViewSalesTable = () => {
     fetchSales();
   }, []);
 
-
-  useEffect(() => {
-    // Fetch sales data from the backend
-    axios.get('http://localhost:5555/sale')
-      .then(response => {
-        setSales(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching sales:', error);
-      });
-  }, []);
-
   const formatDecimal = (decimal128) => {
     if (!decimal128 || !decimal128.$numberDecimal) {
       return '0.00';
@@ -48,28 +37,35 @@ const ViewSalesTable = () => {
       console.error('Error deleting sale:', error);
     }
   };
- // Pagination logic
- const indexOfLastRecord = currentPage * recordsPerPage;
- const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
- const currentRecords = sales.slice(indexOfFirstRecord, indexOfLastRecord);
- const totalPages = Math.ceil(sales.length / recordsPerPage);
 
- const handleNextPage = () => {
-   if (currentPage < totalPages) {
-     setCurrentPage(currentPage + 1);
-   }
- };
+  const handleEditClick = (sale) => {
+    setShowAddForm(true);
+    handleEdit(sale);
+  };
+  
+  // Pagination logic
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = sales.slice(indexOfFirstRecord, indexOfLastRecord);
+  const totalPages = Math.ceil(sales.length / recordsPerPage);
 
- const handlePrevPage = () => {
-   if (currentPage > 1) {
-     setCurrentPage(currentPage - 1);
-   }
- };
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div>
       <table>
-        <thead>
-          <tr>
+      <thead>
+      <tr>
             <th>Ship Date</th>
             <th>Manufacturer</th>
             <th>Sales Unit Price</th>
@@ -79,8 +75,8 @@ const ViewSalesTable = () => {
             <th>Status</th>
             <th>Action</th>
           </tr>
-        </thead>
-        <tbody>
+      </thead>
+      <tbody>
           {currentRecords.map(sale => (
             // eslint-disable-next-line no-underscore-dangle
             <tr key={sale._id}>
@@ -98,18 +94,19 @@ const ViewSalesTable = () => {
                     // eslint-disable-next-line no-underscore-dangle
                     onClick={() => setDropdownVisible(dropdownVisible === sale._id ? null : sale._id)}>...</button>{dropdownVisible === sale._id && (
                     <div className="dropdown-content">
-                      <button type="button" onClick={() => {alert('Edit Clicked');window.location.reload();}}>Edit</button>
+                      <button type="button" onClick={() => {handleEditClick(sale)}}>Edit</button>
                       <button type="button" onClick={() => 
                         // eslint-disable-next-line no-underscore-dangle
-                        handleDeleteClick(sale._id)}>Delete</button>                    </div>
+                        handleDeleteClick(sale._id)}>Delete</button>                    
+                        </div>
                   )}
                 </div>
               </td>
             </tr>
           ))}
         </tbody>
-      </table>
-      <div className="pagination">
+    </table>
+    <div className="pagination">
         <button type="button" onClick={handlePrevPage} disabled={currentPage === 1}>
           &lt;
         </button>

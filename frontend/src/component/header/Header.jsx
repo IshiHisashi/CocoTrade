8a13ea221bbc833b902d8bc83ec52b1041545f91
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-06-20 19:13:40 -0700

    feat: TASK-156 - get company name dynamically

--- frontend/src/component/header/Header.jsx
+++ frontend/src/component/header/Header.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import UserIdContext from "../../page/dashboard/UserIdContext.jsx";
 
 const Header = () => {
-  const [companyName, setCompanyName] = useState();
+  const userId = useContext(UserIdContext);
+  const [companyName, setCompanyName] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(`http://localhost:5555/user/${userId}`);
+      setCompanyName(res.data.data.company_name);
+    })();
+  }, [userId]);
 
   return (
     <header className="bg-lime-300 ml-52 h-24 sticky top-0 flex justify-between items-center p-4">
-      <h2 className="text-4xl">Hello Cocolife</h2>
+      <h2 className="text-4xl">Hello {companyName}</h2>
       <div className="flex gap-4">
         <div className="w-6 h-6 bg-slate-400 text-center rounded-[50%]">N</div>
-        <div className="w-6 h-6 bg-[#0C7F8E] text-center rounded-[50%]">C</div>
+        <div className="w-6 h-6 bg-[#0C7F8E] text-center rounded-[50%]">
+          {companyName.split("")[0]}
+        </div>
       </div>
     </header>
   );

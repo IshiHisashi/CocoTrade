63e8288d54e183ff2995398fbb426a0b6a458498
Akiodesukedo <abtai0227aki@gmail.com>
2024-06-22 15:08:02 -0700

    build: create reusable cta btn

--- frontend/src/page/inventory/Inventory.jsx
+++ frontend/src/page/inventory/Inventory.jsx
@@ -4,21 +4,25 @@ import BarChart from "./BarChart";
 import LineChart from "./LineChart";
 import SalesTable from "./SalesTable";
 import PlanShipment from "./PlanShipment";
+import CtaBtn from "../../component/btn/CtaBtn";
 
 Modal.setAppElement('#root');
 
 const Inventory = () => {
   const [showModal, setShowModal] = useState(false);
   const userId = "66622c07858df5960bf57a06";
+  const fncShowMedal = () => {
+    setShowModal(true);
+  }
   return (
     <div>
       <h1>Inventory</h1>
-      <button type="button" onClick={() => {
-        setShowModal(true);
-        // className="bg-teal-900 text-white"
-      }}>
-        Plan Shipment
-      </button>
+      <CtaBtn 
+        size="M"
+        level="S"
+        innerTxt="Plan shipment"
+        onClickFnc={fncShowMedal}
+      />
       <Modal 
         isOpen={ showModal }
         onRequestClose={() => {

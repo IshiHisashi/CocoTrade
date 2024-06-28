f4c6390b13fdc917af8fcad3858ed6e06c455db1
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-06-27 23:02:32 -0700

    fix: TASK-216 - update the argument of patch method for user docs

--- frontend/src/page/sale/EditSaleModal.jsx
+++ frontend/src/page/sale/EditSaleModal.jsx
@@ -1,76 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Field from '../../component/field-filter/Field'; 
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Field from "../../component/field-filter/Field";
 
 const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
   const navigate = useNavigate();
   const [manufacturers, setManufacturers] = useState([]);
   const [user, setUser] = useState(null);
   const [formData, setFormData] = useState({
-    manufacturer_id: '',
-    amount_of_copra_sold: '', 
-    sales_unit_price: '', 
-    status: '',
-    copra_ship_date: '',
-    cheque_receive_date: '',
-    total_sales_price: '',
-    user_id: '66640d8158d2c8dc4cedaf1e'
+    manufacturer_id: "",
+    amount_of_copra_sold: "",
+    sales_unit_price: "",
+    status: "",
+    copra_ship_date: "",
+    cheque_receive_date: "",
+    total_sales_price: "",
+    user_id: "66640d8158d2c8dc4cedaf1e",
   });
 
-  const userid = '66640d8158d2c8dc4cedaf1e';
+  const userid = "66640d8158d2c8dc4cedaf1e";
 
   useEffect(() => {
     // Fetch user data
-    axios.get(`http://localhost:5555/user/${userid}`)
-      .then(response => {
-        setUser(response.data.data); 
+    axios
+      .get(`http://localhost:5555/user/${userid}`)
+      .then((response) => {
+        setUser(response.data.data);
       })
-      .catch(error => {
-        console.error('Error fetching user:', error);
+      .catch((error) => {
+        console.error("Error fetching user:", error);
       });
 
     // Fetch manufacturers
-    axios.get('http://localhost:5555/manufacturer')
-      .then(response => {
+    axios
+      .get("http://localhost:5555/manufacturer")
+      .then((response) => {
         setManufacturers(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching manufacturers:', error);
+      .catch((error) => {
+        console.error("Error fetching manufacturers:", error);
       });
 
     // Fetch price suggestion
-    axios.get(`http://localhost:5555/user/${userid}/pricesuggestion/getone`)
-      .then(response => {
-        if (response.data.status === 'success') {
+    axios
+      .get(`http://localhost:5555/user/${userid}/pricesuggestion/getone`)
+      .then((response) => {
+        if (response.data.status === "success") {
           setFormData((prevData) => ({
             ...prevData,
-            sales_unit_price: parseFloat(response.data.data?.$numberDecimal ?? response.data.data), 
+            sales_unit_price: parseFloat(
+              response.data.data?.$numberDecimal ?? response.data.data
+            ),
           }));
         }
       })
-      .catch(error => {
-        console.error('Error fetching price suggestion:', error);
+      .catch((error) => {
+        console.error("Error fetching price suggestion:", error);
       });
-
   }, [sale]); // Include purchase in the dependency array
-
 
   useEffect(() => {
     if (sale) {
       setFormData({
         ...sale,
-                          // eslint-disable-next-line no-underscore-dangle 
-            manufacturer_id: sale.manufacturer_id?._id ?? '',
-            amount_of_copra_sold: parseFloat(sale.amount_of_copra_sold?.$numberDecimal ?? sale.amount_of_copra_sold),
-            sales_unit_price: parseFloat(sale.sales_unit_price?.$numberDecimal ?? sale.sales_unit_price),
-            copra_ship_date: sale.copra_ship_date ? new Date(sale.copra_ship_date).toISOString().split('T')[0] : '',
-            cheque_receive_date: sale.cheque_receive_date ? new Date(sale.cheque_receive_date).toISOString().split('T')[0] : '',
-            total_sales_price: parseFloat(sale.total_sales_price?.$numberDecimal ?? sale.total_sales_price),
-        });
+        // eslint-disable-next-line no-underscore-dangle
+        manufacturer_id: sale.manufacturer_id?._id ?? "",
+        amount_of_copra_sold: parseFloat(
+          sale.amount_of_copra_sold?.$numberDecimal ?? sale.amount_of_copra_sold
+        ),
+        sales_unit_price: parseFloat(
+          sale.sales_unit_price?.$numberDecimal ?? sale.sales_unit_price
+        ),
+        copra_ship_date: sale.copra_ship_date
+          ? new Date(sale.copra_ship_date).toISOString().split("T")[0]
+          : "",
+        cheque_receive_date: sale.cheque_receive_date
+          ? new Date(sale.cheque_receive_date).toISOString().split("T")[0]
+          : "",
+        total_sales_price: parseFloat(
+          sale.total_sales_price?.$numberDecimal ?? sale.total_sales_price
+        ),
+      });
     }
   }, [sale]); // Depend on saleId and isOpen to re-run this effect
-  
+
   // Calculate total sales price
   useEffect(() => {
     const calculateTotalSalesPrice = () => {
@@ -78,7 +91,7 @@ const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
       const amountSold = parseFloat(formData.amount_of_copra_sold);
       if (!Number.isNaN(unitPrice) && !Number.isNaN(amountSold)) {
         const total = unitPrice * amountSold;
-        setFormData(prevData => ({
+        setFormData((prevData) => ({
           ...prevData,
           total_sales_price: total.toFixed(2),
         }));
@@ -92,7 +105,7 @@ const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
     const { name, value, type, checked } = e.target;
     setFormData({
       ...formData,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     });
   };
 
@@ -102,20 +115,23 @@ const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
       if (sale) {
         await handleUpdate(formData);
       } else {
-        const response = await axios.post('http://localhost:5555/sale', formData);
-        console.log('Sale created:', response.data);
-                          // eslint-disable-next-line no-underscore-dangle 
+        const response = await axios.post(
+          "http://localhost:5555/sale",
+          formData
+        );
+        console.log("Sale created:", response.data);
+        // eslint-disable-next-line no-underscore-dangle
         const saleId = response.data._id;
 
-        const updatedSalesArray = [...user.sales_array, saleId];
+        // const updatedSalesArray = [...user.sales_array, saleId];
 
         await axios.patch(`http://localhost:5555/user/${userid}`, {
-          sales_array: updatedSalesArray
+          sales_array: saleId,
         });
         setShowAddForm(false);
       }
     } catch (error) {
-      console.error('Error creating/updating purchase:', error);
+      console.error("Error creating/updating purchase:", error);
     }
   };
   return (
@@ -128,8 +144,11 @@ const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
           value={formData.manufacturer_id}
           onChange={handleChange}
           type="dropdown"
-          // eslint-disable-next-line no-underscore-dangle 
-          options={manufacturers.map(manufacturer => ({ value: manufacturer._id, label: manufacturer.full_name }))}
+          options={manufacturers.map((manufacturer) => ({
+            // eslint-disable-next-line no-underscore-dangle
+            value: manufacturer._id,
+            label: manufacturer.full_name,
+          }))}
           required
         />
         <Field
@@ -154,10 +173,10 @@ const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
           value={formData.status}
           onChange={handleChange}
           options={[
-            { value: 'pending', label: 'Pending' },
-            { value: 'ongoing', label: 'Ongoing' },
-            { value: 'completed', label: 'Completed' },
-            { value: 'cancelled', label: 'Cancelled' }
+            { value: "pending", label: "Pending" },
+            { value: "ongoing", label: "Ongoing" },
+            { value: "completed", label: "Completed" },
+            { value: "cancelled", label: "Cancelled" },
           ]}
         />
         <Field
@@ -181,12 +200,19 @@ const EditSaleModal = ({ setShowAddForm, sale, handleUpdate }) => {
           value={formData.total_sales_price}
           onChange={handleChange}
         />
-        <button type="button" onClick={() => {setShowAddForm(false); window.location.reload();}}>Clear</button>
+        <button
+          type="button"
+          onClick={() => {
+            setShowAddForm(false);
+            window.location.reload();
+          }}
+        >
+          Clear
+        </button>
         <button type="submit">Save</button>
       </form>
     </div>
   );
 };
-
 
 export default EditSaleModal;

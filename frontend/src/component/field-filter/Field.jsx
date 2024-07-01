6653d79351c9d64656a15818d975718548c52b05
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-07-01 09:03:51 -0700

    feat: TASK-233 - add button to make disabled input editable

--- frontend/src/component/field-filter/Field.jsx
+++ frontend/src/component/field-filter/Field.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Field = ({
   label,
@@ -9,7 +9,12 @@ const Field = ({
   options,
   disabled = false,
   required = false,
+  showChangeButton = false,
 }) => {
+  const [isDisabled, setIsDisabled] = useState(disabled);
+  const [isShowChangeButton, setIsShowChangeButton] =
+    useState(showChangeButton);
+
   let inputElement = null;
 
   switch (type) {
@@ -25,8 +30,8 @@ const Field = ({
           id={name}
           value={value}
           onChange={onChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          disabled={disabled}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm basis-full focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          disabled={isDisabled}
           required={required}
         />
       );
@@ -55,7 +60,7 @@ const Field = ({
   }
 
   return (
-    <div className="mb-4 w-full">
+    <div className="mb-4 w-full flex flex-wrap justify-between">
       {label && (
         <label
           htmlFor={name}
@@ -63,6 +68,18 @@ const Field = ({
         >
           {label} {required && <span className="text-[#FE2E00]">*</span>}
         </label>
+      )}
+      {isShowChangeButton && (
+        <button
+          type="button"
+          className="block text-sm font-medium text-gray-700 underline"
+          onClick={() => {
+            setIsDisabled(false);
+            setIsShowChangeButton(false);
+          }}
+        >
+          Change
+        </button>
       )}
       {inputElement}
     </div>

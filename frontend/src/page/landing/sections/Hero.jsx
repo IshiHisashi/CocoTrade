e3607d4672e3a57c69cd62e7527e7cf3e246fc4a
IshiHisashi <ihsasihhi@gmail.com>
2024-07-07 18:33:01 -0700

    TASK-274:configure tailwind & add color and typegraphy

--- frontend/src/page/landing/sections/Hero.jsx
+++ frontend/src/page/landing/sections/Hero.jsx
@@ -5,9 +5,7 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center gap-8 bg-slate-800 py-6">
       <div className="texts text-center flex flex-col gap-4">
-        <h2 className="text-white text-[36px]">
-          Streamline Your Copra Trading Operations
-        </h2>
+        <h2 className="text-white">Streamline Your Copra Trading Operations</h2>
         <p className="text-white text-[18px]">
           Improve paperless business transactions and operations with our
           all-in-one web app.{" "}

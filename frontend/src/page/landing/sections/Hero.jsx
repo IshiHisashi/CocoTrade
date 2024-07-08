d9e2d458f868859b7b52891d2332182ca359a5f5
IshiHisashi <ihsasihhi@gmail.com>
2024-07-07 22:57:10 -0700

    TASK-274:build hero section

--- frontend/src/page/landing/sections/Hero.jsx
+++ frontend/src/page/landing/sections/Hero.jsx
@@ -3,25 +3,28 @@ import CtaBtn from "../../../component/btn/CtaBtn";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center gap-8 bg-slate-800 py-6">
+    <div className="flex flex-col items-center bg-slate-800 py-6">
       <div className="texts text-center flex flex-col gap-4">
         <h2 className="text-white display-serif">
           Streamline Your Copra Trading Operations
         </h2>
-        <p className="text-white p-18">
+        <p className="text-white p18 pb-[26px]">
           Improve paperless business transactions and operations with our
           all-in-one web app.{" "}
         </p>
       </div>
-      <div className="btn flex gap-[10px]">
-        <button type="submit" className="rounded border text-white">
+      <div className="btn flex gap-[10px] pb-[47px]">
+        <button
+          type="submit"
+          className=" text-white p16 rounded border w-52 h-14"
+        >
           Download proposal
         </button>
         <CtaBtn innerTxt="Free 14-day trial" size="M" />
       </div>
-      <div className="image-section">
+      <div className="image-section px-[15%] pb-[45px]">
         <img src="./images/hero-image_integrated.png" alt="dashboard" />
-      </div>
+      </div>{" "}
     </div>
   );
 };

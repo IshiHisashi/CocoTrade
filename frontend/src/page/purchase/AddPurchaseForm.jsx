3068e2099ee0f8cafce972686997dbfb7f9628eb
Shehani Wijetunga <77097408+shehani-wijetunga@users.noreply.github.com>
2024-07-12 13:00:39 -0700

    CBT-57 : Fix edit purchase date
    1.Go to edit pirchase
    2.Select date now its fixed

--- frontend/src/page/purchase/AddPurchaseForm.jsx
+++ frontend/src/page/purchase/AddPurchaseForm.jsx
@@ -114,7 +114,7 @@ const AddPurchaseForm = ({
             purchase.total_purchase_price
         ),
         purchase_date: purchase.purchase_date
-          ? new Date(purchase.purchase_date).toLocaleDateString('en-CA').split("T")[0]
+          ? new Date(purchase.purchase_date).toISOString('en-CA').split("T")[0]
           : "",
         user_id: userid,
       });

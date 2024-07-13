4043dd34bbb26da04fbeb0e06bec23e4b78e8469
Akiodesukedo <abtai0227aki@gmail.com>
2024-07-13 16:24:47 -0700

    style: duration selector active behavior

--- frontend/src/component/field-filter/DurationSelecter.jsx
+++ frontend/src/component/field-filter/DurationSelecter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import TabBtn from "../btn/TabBtn";
 
 const DurationSelecter = ({
@@ -7,21 +7,37 @@ const DurationSelecter = ({
   thisYear,
   thisMonth,
 }) => {
-  const handleDurationSelecter = (durationType, durationValue) => {
+  const [activeBtn, setActiveBtn] = useState(null);
+  const handleDurationSelecter = (durationType, durationValue, id) => {
     setDurationType(durationType);
     setDurationValue(durationValue);
+    setActiveBtn(id);
   };
+
+  const buttons = [
+    { id: 1, text: "1M", length: "monthly", pass: thisMonth},
+    { id: 2, text: "1Y", length: "yearly", pass: thisYear},
+  ]
 
   return (
     <div className="flex gap-4">
-      <TabBtn
+      {buttons.map((button) => (
+        <TabBtn
+          key={button.id}
+          innerTxt={button.text}
+          isActive={activeBtn === button.id}
+          onClickFnc={() => handleDurationSelecter(button.length, button.pass, button.id)}
+        />
+      ))}
+
+      {/* <TabBtn
         innerTxt="1M"
         onClickFnc={() => handleDurationSelecter("monthly", thisMonth)}
       />
       <TabBtn
         innerTxt="1Y"
         onClickFnc={() => handleDurationSelecter("yearly", thisYear)}
-      />
+      /> */}
     </div>
   );
 };

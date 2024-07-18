909c0a9cea1be70e3525051dfd21819691b3b8c2
Akiodesukedo <abtai0227aki@gmail.com>
2024-07-18 11:35:06 -0700

    fix: small modification

--- frontend/src/page/setting/Setting.jsx
+++ frontend/src/page/setting/Setting.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import Profile from "./Profile";
 import Preference from "./Preference";
 import Security from "./Security";
@@ -7,15 +8,28 @@ import { UserIdContext } from "../../contexts/UserIdContext.jsx";
 
 const Setting = ({ URL }) => {
   const userId = useContext(UserIdContext);
+  const [userInfo, setUserInfo] = useState(null);
 
+  useEffect(() => {
+    // Fetch user data
+    axios
+      .get(`${URL}/user/${userId}`)
+      .then((response) => {
+        setUserInfo(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+      });
+  }, [userId, URL])
 
   return (
     <>
       <p>Settings Page</p>
-      <Profile userId={userId} URL={URL} />
-      <Preference userId={userId} URL={URL} />
-      <Security userId={userId} URL={URL} />
-      <Billing userId={userId} URL={URL} />
+      <Profile userId={userId} URL={URL} userInfo={userInfo} setUserInfo={setUserInfo} />
+      <Preference userId={userId} URL={URL} userInfo={userInfo} setUserInfo={setUserInfo} />
+      <Security userId={userId} URL={URL} userInfo={userInfo} setUserInfo={setUserInfo} />
+      <Billing userId={userId} URL={URL} userInfo={userInfo} setUserInfo={setUserInfo} />
     </>
   )
 };

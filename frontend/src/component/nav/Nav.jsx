56f834a43179494903117e45490053e830bef6a0
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-07-19 02:36:12 -0700

    fix: add scroll behaviour to nav

--- frontend/src/component/nav/Nav.jsx
+++ frontend/src/component/nav/Nav.jsx
@@ -17,9 +17,9 @@ const Nav = (props) => {
 
   return (
     <nav
-      className={`bg-[#243037] ${translateX} sm:translate-x-0 transition-all sm:transition-none ease-in-out fixed h-screen top-0 bottom-0 left-0 w-full sm:w-64 z-10`}
+      className={`bg-[#243037] ${translateX} sm:translate-x-0 transition-all sm:transition-none ease-in-out fixed h-screen top-0 bottom-0 left-0 w-full sm:w-64 z-10 grid grid-rows-[auto_1fr_auto] overflow-scroll`}
     >
-      <div className="w-full sm:w-64 h-24 flex justify-between sm:justify-center items-center px-8">
+      <div className="w-full sm:w-64 h-24 flex justify-between sm:justify-center items-center px-8 sticky top-0 bg-[#243037]">
         <NavLink
           to="/dashboard"
           onClick={() => fnToToggleNav("-translate-x-full")}
@@ -43,7 +43,7 @@ const Nav = (props) => {
         <NavList page="finances" fnToToggleNav={fnToToggleNav} />
       </ul>
 
-      <div className="bg-[#224F55] rounded-2xl text-white m-8 sm:m-4 px-4 py-6 absolute bottom-0 left-0 right-0 flex sm:block items-start gap-4 max-w-96">
+      <div className="bg-[#224F55] rounded-2xl text-white m-8 sm:m-4 px-4 py-6  bottom-0 left-0 right-0 flex sm:block items-start gap-4 max-w-96">
         <img src={Support} alt="" className="mb-4" aria-hidden />
         <div>
           <h6 className="h3-sans">Need help?</h6>

82a53936785386f9970295a3a292993dca2295fe
IshiHisashi <ihsasihhi@gmail.com>
2024-07-24 17:21:42 -0700

    CBT-287:change background color of footer

--- frontend/src/page/landing/sections/LandingFooter.jsx
+++ frontend/src/page/landing/sections/LandingFooter.jsx
@@ -3,7 +3,7 @@ import CtaBtn from "../../../component/btn/CtaBtn";
 
 const LandingFooter = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row gap-[22px] items-center md:justify-between bg-slate-800 px-[120px] py-[30px] text-bluegreen-100">
+    <div className="flex flex-col-reverse md:flex-row gap-[22px] items-center md:justify-between bg-neutral-600 px-[120px] py-[30px] text-bluegreen-100">
       <div>
         <p className="p14 lg:h4-sans-uppercase">
           &copy; 2024 CocoTrade. All rights reserved.

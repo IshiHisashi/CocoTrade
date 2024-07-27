69dcb11f227f96b9debfaa8620f3dfc15122d2cd
IshiHisashi <ihsasihhi@gmail.com>
2024-07-27 00:08:11 -0700

    CBT-333:added loading screen which is shown until multiple fetching are done

--- frontend/src/component/loading/LoadingScreen.jsx
+++ frontend/src/component/loading/LoadingScreen.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const LoadingScreen = () => {
+const LoadingScreen = ({ children, onClose }) => {
+  // ESC kyedown
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [onClose]);
+
   return (
-    // <div className="h-screen bg-neutral-600 flex justify-center items-center">
-    //   <img src="./images/Logo_loading.svg" alt="" />
-    // </div>
-    <div> </div>
+    <div>
+      <div className="fixed inset-0 bg-opacity-95 h-screen bg-neutral-600 z-50 flex flex-col justify-center items-center ">
+        <img src="./images/Logo_loading.svg" alt="loading icon" />
+        <h4 className="h4-sans-uppercase text-white">Loading...</h4>
+        {children}
+      </div>
+    </div>
   );
 };
 
--- frontend/src/page/finance/Finance.jsx
+++ frontend/src/page/finance/Finance.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-// import UserIdContext from "./UserIdContext.jsx";
+import { useLoading } from "../../contexts/LoadingContext.jsx";
+import LoadingScreen from "../../component/loading/LoadingScreen.jsx";
 import MonthlyActivity from "./MonthlyActivity.jsx";
 import LineChart from "./LineChart.jsx";
 
 const Finance = ({ URL }) => {
+  const { loading } = useLoading();
+
   return (
-    <div className="flex flex-col gap-8 sm:mx-[31px] sm:py-[25px]">
-      <LineChart type="cashflow" URL={URL} />
-      <MonthlyActivity URL={URL} />
-      <LineChart type="market" URL={URL} />
+    <div className="">
+      {/* {loading && <LoadingScreen />} */}
+      <div className="flex flex-col gap-8 sm:mx-[31px] sm:py-[25px]">
+        <LineChart type="cashflow" URL={URL} />
+        <MonthlyActivity URL={URL} />
+        <LineChart type="market" URL={URL} />
+      </div>
     </div>
   );
 };
